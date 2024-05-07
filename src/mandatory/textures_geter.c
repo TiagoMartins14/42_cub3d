979f5f0cf4ef11f7691bcb682028c91e137be17a
tiaferna <tiaferna@student.42.fr>
2024-05-07 19:11:22 +0100

    fixed leak error when there was no path to the texture after the initials in the file

--- src/mandatory/textures_geter.c
+++ src/mandatory/textures_geter.c
@@ -6,7 +6,7 @@
 /*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 17:59:55 by tiaferna          #+#    #+#             */
-/*   Updated: 2024/05/07 18:47:40 by tiaferna         ###   ########.fr       */
+/*   Updated: 2024/05/07 19:09:30 by tiaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,21 @@ static bool	is_valid_texture_path(char *path)
 	return (false);
 }
 
+char    *texture_path(int i, t_map_list *node, char *path)
+{
+    i += 2;
+    while (node->row && ft_iswhitespace(node->row[i]) && (node->row + i)[0] != '\n')
+        i++;
+    if (node->row + i)
+    {
+        path = ft_strldup(node->row + i, ft_strlen(node->row) - i - 1);
+        if (is_valid_texture_path(path) == true)
+            return (path);
+        free(path);
+    }
+    return (NULL);
+}
+
 char	*get_texture_path(t_map *map, t_direction dir_code)
 {
 	t_map_list	*node;
@@ -33,6 +48,7 @@ char	*get_texture_path(t_map *map, t_direction dir_code)
 	char		*direction;
 	int			i;
 
+    path = NULL;
 	direction = get_direction(dir_code);
 	node = map->map_list;
 	while (node)
@@ -43,17 +59,10 @@ char	*get_texture_path(t_map *map, t_direction dir_code)
 		if (ft_strncmp(node->row + i, direction, 3) == 0)
 		{
 			free(direction);
-			i += 2;
-			while (node->row && ft_iswhitespace(node->row[i]))
-				i++;
-			if (node->row + i)
-			{
-				path = ft_strldup(node->row + i, ft_strlen(node->row) - i - 1);
-				if (is_valid_texture_path(path) == true)
-					return (path);
-				free(path);
-				break ;
-			}
+			path = texture_path(i, node, path);
+            if (path)
+                return (path);
+            break ;
 		}
 		node = node->next;
 	}
