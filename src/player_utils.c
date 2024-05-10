426195bb3e4cde0e613350f471f56403e8773811
Tiago Martins <tiago@tiaferna.lan>
2024-05-10 20:04:16 +0100

    fixed function

--- src/player_utils.c
+++ src/player_utils.c
@@ -6,7 +6,7 @@
 /*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 19:07:29 by tiaferna          #+#    #+#             */
-/*   Updated: 2024/05/10 19:38:11 by tiaferna         ###   ########.fr       */
+/*   Updated: 2024/05/10 20:03:41 by tiaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,8 +52,8 @@ int	*starting_coordinate(t_map *map)
 		x = 0;
 		while (map->map_grid[y][x])
 		{
-			if (map->map_grid[y][x] == 'N' || map->map_grid[y][x] == 'N' \
-			|| map->map_grid[y][x] == 'N' || map->map_grid[y][x] == 'N')
+			if (map->map_grid[y][x] == 'N' || map->map_grid[y][x] == 'S' \
+			|| map->map_grid[y][x] == 'E' || map->map_grid[y][x] == 'W')
 			{
 				coordinate[0] = y;
 				coordinate[1] = x;
